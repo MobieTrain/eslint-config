1ef7f1924f0d437ec85bfd1e7fdc26123bdc33e0
Raquel Serrano <raquelserranor@hotmail.com>
2023-01-24 18:49:25 +0100

    chore(linter-rules): enable eslint rule 'jsx-a11y/alt-text' [MT-6235]

--- react-app.js
+++ react-app.js
@@ -5,5 +5,9 @@ module.exports = {
   rules: {
     'react/jsx-pascal-case': ['error', { allowNamespace: true }],
     'no-mixed-operators': 'off',
+    'jsx-a11y/alt-text': [ 2, {
+      'elements': [ 'img', 'object', 'area', 'input[type=\"image\"]' ],
+      'img': ['Media', 'animated.img'],
+    }],
   }
 }
